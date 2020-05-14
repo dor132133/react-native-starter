832f154345ca25881b72c888340c4b50b158901c
dor132133 <dor132133@gmail.com>
2020-05-14 01:31:44 -0700

    done with search bar

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { View, Text, StyleSheet, TextInput} from 'react-native'
 import { Feather } from '@expo/vector-icons' //Feather is the name of the Icon library
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit}) => {
     return (
-        <View style={styles.background}>
-            <Feather name='search' size={30}></Feather>
-            <TextInput></TextInput>
+        <View style={styles.backgroundStyle}>
+            <Feather name='search'style={styles.iconStyle}></Feather>
+            <TextInput
+                autoCapitalize="none"
+                autoCorrect={false}
+                style = {styles.inputStyle}
+                placeholder="Search"
+                value={term}
+                onChangeText={onTermChange}
+                onEndEditing={onTermSubmit}
+            />
         </View>
     )
 }
@@ -14,12 +22,24 @@ const SearchBar = () => {
 
 
 const styles = StyleSheet.create({
-    backgraound: {
+    backgroundStyle: {
         backgroundColor: '#F0EEEE',
         height: 50,
         borderRadius: 5,
         marginHorizontal: 15,
-        marginVertical:15
+        marginTop: 15,
+        flexDirection: 'row'
+    },
+    inputStyle: {
+        // borderColor: 'black',
+        // borderWidth: 1,
+        fontSize: 18,
+        flex: 1
+    },
+    iconStyle : {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 15
     }
 })
 
